e55ce3c455bad48ae30cf9c55ea06cdb7809f17d
Beatriz Romero <beatriz.romero@unosquare.com>
2023-06-08 17:24:42 -0500

    adding change selenium base

--- src/main/java/org/unosquare/base/SeleniumBase.java
+++ src/main/java/org/unosquare/base/SeleniumBase.java
@@ -1,4 +1,15 @@
 package org.unosquare.base;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class SeleniumBase {
+    private WebDriver driver;
+
+    private SeleniumBase(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
 }
